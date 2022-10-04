788cfbea716beebd94348553e5859c994ebd3dfe
vilgudolga <95307871+vilgudolga@users.noreply.github.com>
2022-10-04 21:15:07 +0200

    Change task-6

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#validation-input");
+const dataLength = Number(input.getAttribute.data-length);
+input.addEventListener("blur", () => {
+    if (input.value.length == dataLength) {
+        input.classList.add('valid')
+        input.classList.remove('invalid')
+     } else {
+        input.classList.add('invalid')
+        input.classList.remove('valid')
+     }
+  });
